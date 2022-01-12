0fe6dfb0b7c2a1c426865e793e47feea8b2cfbdf
Omer Irfan <oi222ay@student.lnu.se>
2022-01-12 16:03:17 +0100

    implementing queries to a03

--- Assignment-3/routes/index.js
+++ Assignment-3/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  port: "3306",
+  user: "root",
+  password: "12345678",
+  database: "gamesDB",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to gamesDB!");
+});
+
+con.query("SELECT * FROM user", function (err, result, fields) {
+  if (err) throw err;
+  console.log(result.length);
+  router.get("/", function (req, res, next) {
+    res.render("index", { result });
+  });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
